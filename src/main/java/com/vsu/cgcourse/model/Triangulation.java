08f62eb3d9119890c48d24b77abe5ea74a156d81
Pustoslov <polaris9955@gmail.com>
2022-01-25 11:16:40 +0300

    Fix saving triangulated model

--- src/main/java/com/vsu/cgcourse/model/Triangulation.java
+++ src/main/java/com/vsu/cgcourse/model/Triangulation.java
@@ -6,38 +6,40 @@ public class Triangulation {
     public static ArrayList<Polygon> polygonsTriangulation(ArrayList<Polygon> polygons) {
         ArrayList<Polygon> result = new ArrayList<>();
         for (Polygon currPolygon : polygons) {
-            Polygon triangulatedPolygon = new Polygon();
-            if (currPolygon.getVertexIndexes().size() != 0) {
-                triangulatedPolygon.setVertexIndexes(triangulation(currPolygon.getVertexIndexes()));
-            }
+            ArrayList<ArrayList<Integer>> arrPolygonVertex = triangulation(currPolygon.getVertexIndexes());
+            ArrayList<ArrayList<Integer>> arrPolygonTextureVertex = triangulation(currPolygon.getTextureVertexIndexes());
+            ArrayList<ArrayList<Integer>> arrPolygonNormalVertex = triangulation(currPolygon.getNormalIndexes());
 
-            if (currPolygon.getTextureVertexIndexes().size() != 0) {
-                triangulatedPolygon.setTextureVertexIndexes(triangulation(currPolygon.getTextureVertexIndexes()));
-            }
+            for (int i = 0; i < arrPolygonVertex.size(); i++) {
+                Polygon triangulatedPolygon = new Polygon();
+                triangulatedPolygon.setVertexIndexes(arrPolygonVertex.get(i));
+                if (!arrPolygonTextureVertex.isEmpty() && i < arrPolygonTextureVertex.size()) {
+                    triangulatedPolygon.setTextureVertexIndexes(arrPolygonTextureVertex.get(i));
+                }
+                if (!arrPolygonNormalVertex.isEmpty() && i < arrPolygonNormalVertex.size()) {
+                    triangulatedPolygon.setNormalIndexes(arrPolygonNormalVertex.get(i));
+                }
 
-            if (currPolygon.getNormalIndexes().size() != 0) {
-                triangulatedPolygon.setNormalIndexes(currPolygon.getNormalIndexes());
+                result.add(triangulatedPolygon);
             }
-
-            result.add(triangulatedPolygon);
         }
-
         return result;
     }
 
-    public static ArrayList<Integer> triangulation(ArrayList<Integer> polygon) {
-        ArrayList<Integer> result = new ArrayList<>();
-            int countOfAngles = polygon.size();
-            if (countOfAngles > 3) {
-                for (int j = 0; j < countOfAngles - 2; j++) {
-                    result.add(polygon.get(0));
-                    result.add(polygon.get(j + 1));
-                    result.add(polygon.get(j + 2));
-                }
-            } else {
-                return polygon;
+    public static ArrayList<ArrayList<Integer>> triangulation(ArrayList<Integer> polygon) {
+        ArrayList<ArrayList<Integer>> arrPolygon = new ArrayList<>();
+        int countOfAngles = polygon.size();
+        if (countOfAngles > 3) {
+            for (int j = 0; j < countOfAngles - 2; j++) {
+                ArrayList<Integer> result = new ArrayList<>();
+                result.add(polygon.get(0));
+                result.add(polygon.get(j + 1));
+                result.add(polygon.get(j + 2));
+                arrPolygon.add(result);
             }
-
-        return result;
+        } else {
+            arrPolygon.add(polygon);
+        }
+        return arrPolygon;
     }
 }
